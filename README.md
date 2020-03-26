4a2e72f08c9fa0a6dcbac753b1e6f392b1a1ba45
Pijush Konar <59543014+pijush-konar-au7@users.noreply.github.com>
2020-03-27 01:11:05 +0530

    Update

--- README.md
+++ README.md
@@ -8,3 +8,18 @@ The aim of this project is to build a python program that runs as a command-line
 ### **Approach**
 
 By using the os module of python to recursively list out all the files that are present in the folders with their relative and absolute path then create a new folder with the parent called organized which will contain all the files in organized sub-folders
+
+### **Steps**
+
+We import os and shutil at first
+     The OS module in python provides functions for interacting with the operating system. OS, comes under Python’s standard utility modules. This module provides a portable way of using operating system dependent functionality. The *os* and *os.path* modules include many functions to interact with the file system.
+
+     Shutil module in Python provides many functions of high-level operations on files and collections of files. It comes under Python’s standard utility modules. This module helps in automating the process of copying and removal of files and directories. (Ex - shutil.move() method Recursively moves a file or directory (source) to another location (destination) and returns the destination.)
+
+Then we create the dictionaries for the folders. And the code will have the defined directories for each type of extentions 
+(Example - 'Audio':('.mp3','.wav','.flac', '.m4a', '.aac') Here we define the type of extentions of files for the sub-folder Audio)
+
+Then we start mapping the file formats with directory.
+
+
+
--- file_organizer.py
+++ file_organizer.py
@@ -2,6 +2,7 @@
 
 
 import os, shutil
+
 file_extensions={
        'Audio':('.mp3','.wav','.flac', '.m4a', '.aac'),
        'Video':('.mp4','.mkv','.MKV','.flv','.mpeg'),
@@ -11,7 +12,7 @@ file_extensions={
        'Others':('.exe', '.apk', '.bat', '.bin'),
        'Programming':('.py', '.htm', '.html', '.html5', '.css', '.php', '.js')
 }
-parent_folder={
+new_path={
     "Organized":('Audio','Video','Images','Docs','Archives','Others','Programming')
 }
 
@@ -28,15 +29,6 @@ def file_finder(folderpath,file_extensions):
                 files.append(file)
     return files 
 
-# To Add Size Feature
-
-# def sizecheck(folderpath):
-#     list_dir=os.listdir(folderpath)
-#     for file in list_dir:
-#         if typeofsort== 'size':
-#             if os.path.isfile(folderpath+"/"+file):
-#                 sizeoffile=os.stat(folderpath+"/"+file).st_size
-
 for extensions_type,extension_tuple in file_extensions.items():
     folder_name=extensions_type.split('_')[0]
     folder_path=os.path.join(folderpath,folder_name)
@@ -46,7 +38,7 @@ for extensions_type,extension_tuple in file_extensions.items():
         item_new_path=os.path.join(folder_path,item)
         shutil.move(item_path,item_new_path)
 
-for extensions_type,extension_tuple in parent_folder.items():
+for extensions_type,extension_tuple in new_path.items():
     folder_name=extensions_type.split('_')[0]
     folder_path=os.path.join(folderpath,folder_name)
     os.mkdir(folder_path)
@@ -54,3 +46,22 @@ for extensions_type,extension_tuple in parent_folder.items():
         item_path=os.path.join(folderpath,item)
         item_new_path=os.path.join(folder_path,item)
         shutil.move(item_path,item_new_path)
+
+
+# To Add Size Feature
+
+# def sizecheck(folderpath):
+#     list_dir=os.listdir(folderpath)
+#     for file in list_dir:
+#         sizeoffile=os.stat(file).st_size
+#         try:
+#             if sizeoffile<1000:
+#                 os.makedirs(folderpath+Bytes)
+#                 shutil.move(folderpath+files, path+"Bytes/"+file)
+#                 continue
+#         except FileExistsError:
+#             continue
+
+# # func_to_call = {'ext':file_finder, 'size':sizecheck }
+# func_to_call = {'size':sizecheck }
+# func_to_call[typeofsort]()
